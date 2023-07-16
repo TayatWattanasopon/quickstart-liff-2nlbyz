a1134d5afcfefa0ea0fa6a8e2e616bd166daaf58
TayatWattanasopon <113895105+TayatWattanasopon@users.noreply.github.com>
2023-07-16 09:56:57 +0700

    2 files modified

--- index.js
+++ index.js
@@ -1,60 +1,72 @@
 // Import stylesheets
-import "./style.css"
+import './style.css';
 
 // Body element
-const body = document.getElementById('body')
+const body = document.getElementById('body');
 
 // Button elements
-const btnSend = document.getElementById("btnSend")
-const btnClose = document.getElementById("btnClose")
-const btnShare = document.getElementById("btnShare")
-const btnLogIn = document.getElementById("btnLogIn")
-const btnLogOut = document.getElementById("btnLogOut")
-const btnScanCode = document.getElementById("btnScanCode")
-const btnOpenWindow = document.getElementById("btnOpenWindow")
+const btnSend = document.getElementById('btnSend');
+const btnClose = document.getElementById('btnClose');
+const btnShare = document.getElementById('btnShare');
+const btnLogIn = document.getElementById('btnLogIn');
+const btnLogOut = document.getElementById('btnLogOut');
+const btnScanCode = document.getElementById('btnScanCode');
+const btnOpenWindow = document.getElementById('btnOpenWindow');
 
 // Profile elements
-const email = document.getElementById("email")
-const userId = document.getElementById("userId")
-const pictureUrl = document.getElementById("pictureUrl")
-const displayName = document.getElementById("displayName")
-const statusMessage = document.getElementById("statusMessage")
+const email = document.getElementById('email');
+const userId = document.getElementById('userId');
+const pictureUrl = document.getElementById('pictureUrl');
+const displayName = document.getElementById('displayName');
+const statusMessage = document.getElementById('statusMessage');
 
 // QR element
-const code = document.getElementById("code")
-const friendShip = document.getElementById("friendShip")
+const code = document.getElementById('code');
+const friendShip = document.getElementById('friendShip');
 
 async function main() {
   // Initialize LIFF app)
-  await liff.init({ liffId: "2000141235-kLKnmg5M" })
+  await liff.init({ liffId: '2000141235-kLKnmg5M' });
   // Try a LIFF function
   switch (liff.getOS()) {
-    case "android": body.style.backgroundColor = "#d1f5d3"; break
-    case "ios": body.style.backgroundColor = "#eeeeee"; break
+    case 'android':
+      body.style.backgroundColor = '#d1f5d3';
+      break;
+    case 'ios':
+      body.style.backgroundColor = '#eeeeee';
+      break;
   }
-  getUserProfile()
+  getUserProfile();
   if (!liff.isInClient()) {
-    btnLogIn.style.display = "block"
-    btnLogOut.style.display = "block"
+    if (liff.isLoggedIn()) {
+      btnLogIn.style.display = 'none';
+      btnLogOut.style.display = 'block';
+      getUserProfile();
+    } else {
+      btnLogIn.style.display = 'block';
+      btnLogOut.style.display = 'none';
+    }
+  } else {
+    getUserProfile();
   }
 }
 
 async function getUserProfile() {
-  const profile = await liff.getProfile()
-  pictureUrl.src = profile.pictureUrl
-  userId.innerHTML = "<b>userId:</b> " + profile.userId
-  statusMessage.innerHTML = "<b>statusMessage:</b> " + profile.statusMessage
-  displayName.innerHTML = "<b>displayName:</b> " + profile.displayName
-  email.innerHTML = "<b>email:</b> " + liff.getDecodedIDToken().email
+  const profile = await liff.getProfile();
+  pictureUrl.src = profile.pictureUrl;
+  userId.innerHTML = '<b>userId:</b> ' + profile.userId;
+  statusMessage.innerHTML = '<b>statusMessage:</b> ' + profile.statusMessage;
+  displayName.innerHTML = '<b>displayName:</b> ' + profile.displayName;
+  email.innerHTML = '<b>email:</b> ' + liff.getDecodedIDToken().email;
 }
 
-main()
+main();
 
 btnLogIn.onclick = () => {
-  liff.login()
-}
+  liff.login();
+};
 
 btnLogOut.onclick = () => {
-  liff.logout()
-  window.location.reload()
-}
+  liff.logout();
+  window.location.reload();
+};
